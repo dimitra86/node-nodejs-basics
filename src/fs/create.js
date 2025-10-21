1d9750f3e011e55d1be75bda26746047106fd614
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-10-21 22:36:11 +0300

    feat:  implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
-const create = async () => {
-  // Write your code here
-};
+import { writeFile, existsSync } from 'node:fs';
+import { promises as fs } from 'node:fs';
 
-await create();
+const create = async () => {
+  try {
+    const filePath = './src/fs/files/fresh.txt';
+    if (existsSync(filePath)) {
+      throw new Error(`File ${filePath} already exists.`);
+    }
+    const content = 'I am fresh and young';
+    await fs.writeFile(filePath, content);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+create();
