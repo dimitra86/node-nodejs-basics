92519b855a61ac5fa4056a8209bb798c954bf078
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-10-26 17:55:26 +0300

    feat:  implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+import { argv } from 'process';
+
 const parseArgs = () => {
-  // Write your code here
+  const args = argv.slice(2);
+  const parsedArgs = {};
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].replace('--', '');
+    parsedArgs[propName] = args[i + 1];
+  }
+
+  Object.entries(parsedArgs).forEach(([key, value]) => {
+    console.log(`${key} is ${value}`);
+  });
 };
 
 parseArgs();
