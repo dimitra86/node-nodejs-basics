da1ec0db052092f42e4bc81235258169e474ffeb
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-10-26 11:04:21 +0300

    feat: implement function that deletes file fileToRemove.txt (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,10 @@
+import { unlink } from 'node:fs';
+
 const remove = async () => {
-  // Write your code here
+  unlink('./src/fs/files/fileToRemove.txt', (err) => {
+  if (err) throw err;
+  console.log('./src/fs/files/fileToRemove.txt');
+}); 
 };
 
 await remove();
