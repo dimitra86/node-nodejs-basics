703cd4fcd1c4a21612484e05b7b90fc4f5fbc353
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-10-26 15:28:22 +0300

    feat: implement function that copies folder files files with all its content into folder files_copy at the same level (if files folder doesn't exist or files_copy has already been created Error with message FS operation failed must be thrown)

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import { promises as fs } from 'fs';
+
 const copy = async () => {
-  // Write your code here
+  try {
+    const src = './src/fs/files';
+    const dest = './src/fs/files_copy';
+
+    try {
+      await fs.access(src);
+    } catch {
+      throw new Error('FS operation failed');
+    }
+
+    try {
+      await fs.access(dest);
+      throw new Error('FS operation failed');
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw err;
+      }
+    }
+
+    await fs.cp(src, dest, { recursive: true });
+    console.log('Directory copied successfully');
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
-await copy();
+try {
+  await copy();
+} catch (err) {
+  console.error(err.message);
+}
