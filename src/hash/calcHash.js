8fac23a3419f63742235f170d1764da309e61516
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-10-26 18:29:29 +0300

    feat: implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex using Streams API

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-  // Write your code here
+  const filePath = './src/hash/files/fileToCalculateHashFor.txt';
+
+  const hash = createHash('sha256');
+
+  const fileStream = createReadStream(filePath);
+
+  fileStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on('end', () => {
+    const hexHash = hash.digest('hex');
+    console.log(hexHash);
+  });
 };
 
-await calculateHash();
+calculateHash();
